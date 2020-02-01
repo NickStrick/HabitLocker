3dbc7f79c006fada8b087c473d8fe0619456db17
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-31 19:48:30 -0600

    sidebar component and styling added

---
+++ habitapp/src/components/NavBelt/NavBelt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBelt.scss';
+
+const NavBelt = () => {
+    return (
+        <div className="NavBelt">
+            <h2>Planning</h2>
+            <h2>Daily</h2>
+            <h2>Monthly</h2>
+            <h2>Custom</h2>
+        </div>
+    );
+}
+
+export default NavBelt;
