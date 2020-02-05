0d4703c85cf2f0a7027f8ee24ce87594fa6a3c33
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-04 19:01:57 -0600

    sidebar classswapping working"

--- habitapp/src/components/NavBelt/NavBelt.js
+++ habitapp/src/components/NavBelt/NavBelt.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavBelt.scss';
 
 const NavBelt = () => {
+
+    const [pinText, setPinText] = useState('down');
+    const [scrollNum, setScroll] = useState(window.scrollY);
+
+    function updateScroll() {
+        if (window.scrolly !== scrollNum) {
+            setScroll(window.scrollY)
+        }
+
+    }
+
+    window.addEventListener('scroll', () => updateScroll());
+
+    useEffect(() => {
+        if (window.scrollY < 47) {
+            if (pinText != 'down') {
+                setPinText('down');
+            }
+        } else {
+            if (pinText != 'up') {
+                setPinText('up');
+            }
+        }
+    });
+
     return (
-        <div className="NavBelt">
+        <div className={`NavBelt ${pinText}`}>
             <h2>Planning</h2>
             <h2>Daily</h2>
             <h2>Monthly</h2>
