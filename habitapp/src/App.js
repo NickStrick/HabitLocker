a1f51baa52f1d5411c9ce267548698528ce51d06
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-05 18:53:47 -0600

    app styling and react router inplemented

--- habitapp/src/App.js
+++ habitapp/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Route } from "react-router-dom";
 
 import NavBar from './components/Navbar/NavBar.js';
 import NavBelt from './components/NavBelt/NavBelt.js';
+import Home from './components/Home/Home.js';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <NavBar />
       <NavBelt />
       <div className='container'>
-
+        <div className='sidegap'></div>
+        <Route exact path='/' render={props => <Home {...props} />} />
       </div>
     </div>
   );
