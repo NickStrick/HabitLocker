8ec398a5de1a96e9d2fae2a33153c713704ddc1b
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-05 18:32:14 -0600

    home page component and styling added

---
+++ habitapp/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = (props) => {
+
+
+    return (
+        <div className="Home">
+            qoq
+
+        </div>
+    );
+}
+
+export default Home;
