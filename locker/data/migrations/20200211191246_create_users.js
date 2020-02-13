f370c1dbb4531fba318e62478798b2e14573a3af
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-13 16:29:25 -0600

    added users database and seed file

--- locker/data/migrations/20200211191246_create_users.js
+++ locker/data/migrations/20200211191246_create_users.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+
+        tbl.string('username', 255).notNullable().unique();
+        tbl.string('password', 255).notNullable();
+        tbl.string('email', 255)
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
 };
