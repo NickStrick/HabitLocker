cf8b0ec5cbb29d5b6555b3a6fe0060f7bb4d9cf7
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-13 17:28:34 -0500

    posts route added

--- locker/api/server.js
+++ locker/api/server.js
@@ -5,11 +5,15 @@ const helmet = require('helmet');
 
 const server = express();
 
+const postsRouter = require('./Routers/posts/posts-router.js');
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
 //routes
+server.use("/posts", postsRouter);
+
 server.get('/', (req, res) => {
     res.send('HabitLocker Api Working');
 })
