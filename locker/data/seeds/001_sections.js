7ab2f7fbec24378fd63f33301f8073db80ec300a
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-12 21:11:47 -0500

    section seed data

--- locker/data/seeds/001_sections.js
+++ locker/data/seeds/001_sections.js
@@ -8,7 +8,27 @@ exports.seed = async function (knex, Promise) {
         {
           name: 'rocket leauge',
           category: 1
-        }
+        },
+        {
+          name: 'carrer',
+          category: 1
+        },
+        {
+          name: 'personal',
+          category: 1
+        },
+        {
+          name: 'relationships',
+          category: 1
+        },
+        {
+          name: 'interrelations',
+          category: 1
+        },
+        {
+          name: 'foods',
+          category: 1
+        },
       ]);
     });
 };
