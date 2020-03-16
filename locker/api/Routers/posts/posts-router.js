c110c8ef6b40c258e234bff47d1f0c2e897726c8
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-16 17:20:50 -0500

    getpostbysection router function

--- locker/api/Routers/posts/posts-router.js
+++ locker/api/Routers/posts/posts-router.js
@@ -6,6 +6,8 @@ module.exports = router;
 
 //routes
 router.get('/', getPosts);
+router.get('/id/:id', getPostsById);
+router.get('/:section', getPostsBySec);
 
 //Get All Posts.
 function getPosts(req, res) {
@@ -23,7 +25,7 @@ function getPosts(req, res) {
 }
 
 function getPostsById(req, res) {
-    let { id } = req.body;
+    let { id } = req.params;
     db.getPosts(id)
         .then(posts => {
             if (posts.length > 0) {
@@ -37,3 +39,17 @@ function getPostsById(req, res) {
         })
 }
 
+function getPostsBySec(req, res) {
+    let { section } = req.params;
+    db.getPostsBySection(section)
+        .then(posts => {
+            if (posts.length > 0) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({ message: "That section's posts are not available" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+}
