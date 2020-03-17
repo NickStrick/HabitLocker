76473483ac3af696a86518124f25b4494773645a
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-16 21:17:33 -0500

    section by category route

--- locker/api/Routers/sections/sections-router.js
+++ locker/api/Routers/sections/sections-router.js
@@ -3,3 +3,22 @@ const router = require("express").Router();
 const db = require("./sections-model.js");
 
 module.exports = router;
+
+//routes
+router.get('/:category', getSections);
+
+function getSections(req, res) {
+    let { category } = req.params;
+
+    db.getSectionsByCat(category)
+        .then(posts => {
+            if (posts.length > 0) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({ message: "There are no available sections." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+}
