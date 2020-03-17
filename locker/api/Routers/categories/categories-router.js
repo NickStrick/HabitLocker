f7544f1683ba8cb312f7009f5734a854b638fcdc
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-16 21:16:00 -0500

    categories by user route

--- locker/api/Routers/categories/categories-router.js
+++ locker/api/Routers/categories/categories-router.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
 
-const db = require("./categories-route.js");
+const db = require("./categories-model.js");
 
 module.exports = router;
+
+router.get('/:user_id', getCategories);
+
+function getCategories(req, res) {
+    let { user_id } = req.params;
+
+    db.getCategoriesByUser(user_id)
+        .then(posts => {
+            if (posts.length > 0) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({ message: "There are no available categories." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+}
