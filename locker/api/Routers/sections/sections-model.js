4d9246b9cde64411682eea6b4f08372bdfe6b928
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-21 16:45:17 -0500

    sections update and remove

--- locker/api/Routers/sections/sections-model.js
+++ locker/api/Routers/sections/sections-model.js
@@ -15,3 +15,19 @@ function add(section) {
     return db('sections')
         .insert(section, '*')
 }
+
+//Updates a single category
+
+function update(id, changes) {
+    return db('sections')
+        .where({ id })
+        .update(changes, '*')
+}
+
+//Removes a single category by id
+
+function remove(id) {
+    return db('sections')
+        .where({ id })
+        .del();
+}
