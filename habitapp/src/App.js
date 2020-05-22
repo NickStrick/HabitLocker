b462f1f40b455ac6743714da79f0bab8e2a450e2
Nick Stricker <strickerdev@gmail.com>
2020-05-21 21:28:55 -0500

    first backend fetch added to frontend

--- habitapp/src/App.js
+++ habitapp/src/App.js
@@ -2,12 +2,28 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { Route } from "react-router-dom";
+import axios from 'axios';
+// require('dotenv').config();
 
 import NavBar from './components/Navbar/NavBar.js';
 import NavBelt from './components/NavBelt/NavBelt.js';
 import Home from './components/Home/Home.js';
 
 function App() {
+  const [getData, setData] = useState();
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `${process.env.BACKEND_URL}/category/1`,
+      );
+ 
+      setData(result.data);
+    };
+ 
+    fetchData();
+  });
+  console.log(getData)
   return (
     <div className="App">
       <NavBar />
