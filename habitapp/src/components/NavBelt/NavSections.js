b5f1a6c136183b0dc6a59bfba3421652f3558943
Nick Stricker <strickerdev@gmail.com>
2020-05-27 22:28:15 -0500

    renamed and refactored navInfo to include fetched data

---
+++ habitapp/src/components/NavBelt/NavSections.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const NavInfo = (props) => {
+    const [sectionList, setSections] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(
+            `${process.env.REACT_APP_BACKEND_URL}/sections/${props.catId}`,
+          );
+     
+          setSections(result.data);
+        };
+     
+        fetchData();
+      },[]);
+
+    console.log(sectionList)
+    return (
+        <div className="nav-info">
+            {/* {props.items.map((item) => {
+                return (
+                    <p>{item}</p>
+                )
+            })} */}
+        </div>
+    );
+}
+
+export default NavInfo;
