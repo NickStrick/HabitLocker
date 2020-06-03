c2a72e10d603f847cba3e61bb491ab5261901464
Nick Stricker <strickerdev@gmail.com>
2020-06-02 22:11:09 -0500

    postspage component created

---
+++ habitapp/src/components/Posts/PostsPage.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import './PostsPage.scss';
+
+const PostsPage = (props) => {
+
+
+    return (
+        <div className="PostsPage">
+            <p>wow</p>
+
+        </div>
+    );
+}
+
+export default PostsPage;
